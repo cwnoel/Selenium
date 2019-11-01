57b7fc9b2797fcf0483bbbd176c7d3a6b2dac69b
colinnoel <colinnoel@hotmail.com>
2019-11-01 00:59:09 -0700

    First Commit of Selenium Framework

---
+++ src/main/java/resources/OptionsRetriever.java
@@ -0,0 +1,53 @@
+package resources;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+
+public class OptionsRetriever {
+
+	private PropRetriever prop = new PropRetriever();
+
+
+
+public ChromeOptions getChromeOptions() {
+	
+	ChromeOptions options = new ChromeOptions(); 
+	if (prop.getProp("headless").contains("true")) {
+		options.setHeadless(true);
+	}
+	options.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+	options.setCapability(CapabilityType.PLATFORM_NAME, Platform.WINDOWS);
+	return options;
+}
+
+public FirefoxOptions getFireFoxOptions() {
+	
+	FirefoxOptions options = new FirefoxOptions(); 
+	if (prop.getProp("headless").contains("true")) {
+		options.setHeadless(true);
+	}
+	options.setCapability(CapabilityType.BROWSER_NAME, BrowserType.FIREFOX);
+	options.setCapability(CapabilityType.PLATFORM_NAME, Platform.WINDOWS);
+	return options;
+}
+
+
+public ChromeOptions getEdgeOptions() {
+	
+	ChromeOptions options = new ChromeOptions(); 
+	options.setBinary(prop.getProp("edgeBinary"));
+	if (prop.getProp("headless").contains("true")) {
+		options.setHeadless(true);
+	}
+	options.setCapability(CapabilityType.BROWSER_NAME, BrowserType.EDGE);
+	options.setCapability(CapabilityType.PLATFORM_NAME, Platform.WINDOWS);
+	return options;
+}
+
+
+
+
+}
---
+++ target/classes/DriverFactory.java
@@ -0,0 +1,69 @@
+package resources;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class DriverFactory {
+
+	private ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+
+	public synchronized void newDriver() {
+
+		final PropRetriever prop = new PropRetriever();
+		final String browserName = prop.getProp("browser");
+		//final String browserName = System.getProperty("browser");
+		final String remote = prop.getProp("remote");
+		final String grid = prop.getProp("grid");
+		final OptionsRetriever optRet = new OptionsRetriever();
+	
+		
+
+		if (browserName.contains("chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir").toString()
+					+ prop.getProp("chromedriver"));
+			ChromeOptions options = optRet.getChromeOptions();
+			if (remote.contains("true")) {
+				try {
+					driver.set(new RemoteWebDriver(new URL(grid),options));
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else {
+		
+			driver.set(new ChromeDriver(options));
+			}
+		} else if (browserName.contains("firefox")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir").toString()
+					+ prop.getProp("firefoxdriver"));
+			FirefoxOptions options = optRet.getFireFoxOptions();
+			driver.set(new FirefoxDriver(options));
+		}else if (browserName.contains("edge")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir").toString()
+					+ prop.getProp("msedgedriver"));
+			ChromeOptions options = optRet.getEdgeOptions();
+			driver.set(new ChromeDriver(options));
+		}
+		driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	}
+
+	// USE THIS FOR GRID
+//  try {
+//      drivers.set(new RemoteWebDriver(new URL(grid),options));
+//  } catch (MalformedURLException e) {
+//      e.printStackTrace();
+//  }
+
+	public synchronized WebDriver getDriver() {
+		return driver.get();
+	}
+}
