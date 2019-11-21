7cfe943fe5aee242f165b0ad467fddff98196cbb
colin <colinnoel@hotmail.com>
2019-11-21 12:32:43 -0800

    fixed some unused imports

--- src/main/java/pageObjects/InteractionsSectionPage.java
+++ src/main/java/pageObjects/InteractionsSectionPage.java
@@ -1,12 +1,10 @@
 package pageObjects;
 
 import java.util.ArrayList;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InteractionsSectionPage {
 	
